1ac1cabc7d82007bf199448309b2f62ae807ce95
Salome2731 <salomequiceno1127173@correo.itm.edu.co>
2026-05-22 13:40:49 -0500

    Main, correpcion de app

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import {ThemeProvider} from './context/ThemeContext.jsx'
 createRoot(document.getElementById('root')).render(
     <StrictMode>
             <ThemeProvider>
+                <App/>
             </ThemeProvider>
     </StrictMode>
 )
