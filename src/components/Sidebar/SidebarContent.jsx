c8517d780094b7ef0c3a93d87eebc6ac86189c01
Salome2731 <salomequiceno1127173@correo.itm.edu.co>
2026-05-22 13:55:05 -0500

    Cambio de nombre del dashboard

--- src/components/Sidebar/SidebarContent.jsx
+++ src/components/Sidebar/SidebarContent.jsx
@@ -8,7 +8,7 @@ function SidebarContent() {
     return (
         <div className="py-4 text-gray-500 dark:text-gray-400">
             <a className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
-                Nexzus Dashboard
+                Gestión Biblioteca
             </a>
             <ul className="mt-6">
                 {routes.map((route) =>
