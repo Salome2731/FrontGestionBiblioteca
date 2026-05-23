1748eb2d98e71e63edd678ad2e721d915dfe7e25
Salome2731 <salomequiceno1127173@correo.itm.edu.co>
2026-05-23 02:13:06 -0500

    Corección formato jsx en ReservasPage.jsx

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 import { lazy } from 'react'
-import ReservasPage from '../pages/reservas/ReservasPage';
 
 const Page404 = lazy(() => import('../pages/Page404.jsx'))
 const Users = lazy(() => import('../pages/Dashboard/users/UsersPage.jsx'))
