46e4240b3b2eacf4ea683f53770e28d3d9a26057
rrolfyy <rolfypalomino1129372@correo.itm.edu.co>
2026-05-23 00:05:47 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import { lazy } from 'react'
+import ReservasPage from '../pages/reservas/ReservasPage';
 
 const Page404 = lazy(() => import('../pages/Page404.jsx'))
 const Users = lazy(() => import('../pages/Dashboard/users/UsersPage.jsx'))
